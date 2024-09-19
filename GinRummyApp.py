b649a860bbd97a3ec446765c5b62401c224811bf
WallaceGavin <Gavin.r.wallace@outlook.com>
2024-09-18 21:57:59 -0500

    Create GinRummyApp.py
    Started the app development, no code inserted yet.

---
+++ GinRummyApp.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+#Gin Rummy Application
+#Be able to generate a 52 card deck and shuffle to two players
+#Give logic and allow picking of options
+#BONUS: allow the program to learn from mistakes - a little ML
